79cfe2b0a29b97bfab397bc14baa6f4404622ab6
TriTry Guy <tritryguy@gmail.com>
2018-11-04 20:55:36 -0500

    pip-count added

---
+++ pip-count.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# pipCount
+
+print("\n\n\t\t\tzbar1\n\n")
+
+principale=int(input("\t$$$$ >>>  "))
+pips=int(input("\tpips >>>  "))
+
+gains = principale * (pips/1000)
+
+print(gains)
+
+# add "spread" and overhead charges
+# pipeline for real time tracking
